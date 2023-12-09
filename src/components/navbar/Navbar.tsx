bdde4c1f7e540ae32efe21481e3f0185d7fd9dee
Khaled Mohamed Mowafy <45627560+KhaledMowafy@users.noreply.github.com>
2023-12-09 02:00:20 +0200

    fix(tailwind): remove width and add bg colors

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -67,14 +67,14 @@ function Navbar() {
         <>
         
             <nav
-                className={`bg-[#0e1623] bg-opacity-75 absolute left-0 top-0 z-[1035] h-full w-3/12  overflow-hidden bg-white shadow-[0_4px_12px_0_rgba(0,0,0,0.07),_0_2px_4px_rgba(0,0,0,0.05)] transition-all duration-200 `}
+                className={`bg-[#0e1623] bg-opacity-75 absolute left-0 top-0 z-[1035] h-full w-3/12  overflow-hidden shadow-[0_4px_12px_0_rgba(0,0,0,0.07),_0_2px_4px_rgba(0,0,0,0.05)] transition-all duration-200 `}
             >
                 <div className="w-full flex justify-center">
                     <img src={logo} className="w-6/12 mt-3"/>
                 </div>
                 <div className="flex flex-col w-full justify-around mt-3 items-center">
                 <input
-                    className="shadow h-10 appearance-none border rounded w-full mt-3  px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline w-full text-sm  w-4/6"
+                    className="h-10 border rounded mt-3  px-3 text-gray-700 leading-tight focus:outline-none  text-sm w-4/6"
                     id="country"
                     type="text"
                     placeholder="Country"
